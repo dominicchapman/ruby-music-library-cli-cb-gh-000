42fc10c9919f305cacc718a03fa0432cf41a68db
Dominic Chapman <dominicchapman@me.com>
2017-10-15 14:29:38 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(artist_name)
+    artist = self.new(artist_name)
+    artist.save
+    artist
+  end
+
+end
