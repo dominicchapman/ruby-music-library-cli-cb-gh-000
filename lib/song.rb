cc9daa92c1197e4bf69519aba0419c3d9c74c75f
Dominic Chapman <dominicchapman@me.com>
2017-10-15 14:50:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist if artist
   end
