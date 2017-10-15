a42b68c303ebbfd611522c5daee59f4facf85e3d
Dominic Chapman <dominicchapman@me.com>
2017-10-15 15:16:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,8 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
 
-  def self.find_by_name(name)
-    @@all.find(name)
+  def self.find_by_name
+    @@all.find(self.name)
   end
 
 end
