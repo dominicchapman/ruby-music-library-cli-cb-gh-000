15a0f6b98d7cd8e97689388804de4ca7d3db74ab
Dominic Chapman <dominicchapman@me.com>
2017-10-15 14:39:45 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,7 +31,7 @@ class Artist
   end
 
   def add_song(song)
-    song.artist = self unless song.artist.include?(song)
+    song.artist = self unless song.artist
     @songs << song unless @songs.include?(song)
   end
 
